ff4428456e61d13fca37e691b2411876cbe13f90
Kotha Mukesh Raj <134232672+KothaMukeshRaj@users.noreply.github.com>
2023-06-03 02:49:30 +0530

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,18 @@
+function addToResult(value) {
+    document.getElementById('result').value += value;
+}
+
+function clearResult() {
+    document.getElementById('result').value = '';
+}
+
+function deleteLastChar() {
+    var result = document.getElementById('result').value;
+    document.getElementById('result').value = result.slice(0, -1);
+}
+
+function calculate() {
+    var result = document.getElementById('result').value;
+    var calculation = eval(result);
+    document.getElementById('result').value = calculation;
+}
